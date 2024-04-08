ecd5079e4b45cccfbfb88e3fae0902cc1e9039e0
megfree <meg.work.996@gmail.com>
2024-04-08 05:23:48 +0300

    cleanup, fix deploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import svgLoader from 'vite-svg-loader';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader()],
-  base: process.env.NODE_ENV === 'production' ? 'https://megfree.github.io/vue-users-list/' : '',
+  base: process.env.NODE_ENV === 'production' ? '/vue-users-list/' : './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
